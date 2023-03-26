f91818e0defb99ab5d50920ec41ba6c3b474289e
ash.blade <ashen.blade.dev@gmail.com>
2023-03-26 18:52:28 +0300

    Добавил отправку клавиатуры для перехода по ссылке

--- src/services/track_sender/telegram_track_sender/telegram_track_sender.py
+++ src/services/track_sender/telegram_track_sender/telegram_track_sender.py
@@ -1,6 +1,7 @@
 import logging
 
 from aiogram import Bot
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from abstractions.track_sender import TrackSender
 from models import Track
@@ -8,6 +9,21 @@ from services.track_sender.telegram_track_sender.track_formatter import format_t
 
 
 _logger = logging.getLogger(__file__)
+
+
+def create_reply_source_url_keyboard(track: Track):
+    if not track.sources:
+        return
+    keyboard = InlineKeyboardMarkup(row_width=1)
+    for button in [
+        InlineKeyboardButton(
+            text=source.provider.name,
+            url=source.url
+        )
+        for source in track.sources
+    ]:
+        keyboard.add(button)
+    return keyboard
 
 
 class TelegramTrackSender(TrackSender):
@@ -19,7 +35,13 @@ class TelegramTrackSender(TrackSender):
 
     async def send_track(self, track: Track):
         formatted_message = format_track_markdown(track)
+        keyboard = create_reply_source_url_keyboard(track)
         try:
-            await self.bot.send_message(self.chat_id, formatted_message, parse_mode='Markdown')
+            await self.bot.send_message(
+                self.chat_id,
+                formatted_message,
+                parse_mode='Markdown',
+                reply_markup=keyboard
+            )
         except Exception as e:
             _logger.error('Ошибка во время отправки сообщения в чат %i', self.chat_id, exc_info=e)
