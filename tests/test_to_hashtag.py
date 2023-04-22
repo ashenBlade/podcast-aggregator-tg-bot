801a01ca7342fdf5ef3de745ae200f76786fb031
ash.blade <ashen.blade.dev@gmail.com>
2023-04-22 19:47:08 +0300

    Обновил логику превращения слова в хэштег

--- tests/test_to_hashtag.py
+++ tests/test_to_hashtag.py
@@ -6,9 +6,9 @@ from insfrastructure.utils import to_hashtag
 @pytest.mark.parametrize('text,expected', [
     ('asdf', 'asdf'),
     ('a', 'a'),
-    ('ЯндексПодкасты', 'ЯндексПодкасты'),
-    ('GoogleПодкасты', 'GoogleПодкасты'),
-    ('GoogleПодкасты2', 'GoogleПодкасты2'),
+    ('Яндекс Подкасты', 'ЯндексПодкасты'),
+    ('Google Подкасты', 'GoogleПодкасты'),
+    ('Google Подкасты 2', 'GoogleПодкасты2'),
     ('Подкаст69', 'Подкаст69'),
 ])
 def test__when_all_alphanumeric_chars__should_return_same_string(text, expected):
@@ -23,5 +23,21 @@ def test__when_all_alphanumeric_chars__should_return_same_string(text, expected)
     ('Пилим,Трем', 'ПилимТрем')
 ])
 def test__should_remove_non_alphanumeric(text, expected):
+    actual = to_hashtag(text)
+    assert actual == expected
+
+
+@pytest.mark.parametrize('text, expected', [
+    ('Яндекс подкасты', 'ЯндексПодкасты'),
+    ('Мысли и методы', 'МыслиИМетоды'),
+    ('Завтракаст', 'Завтракаст'),
+    ('Разбор полетов', 'РазборПолетов'),
+    ('IT Way Podcast', 'ITWayPodcast'),
+    ('Moscow python', 'MoscowPython'),
+    ('Как делают игры', 'КакДелаютИгры'),
+    ('Релиз в пятницу', 'РелизВПятницу'),
+    ('Веб-стандарты', 'ВебСтандарты')
+])
+def test__should_capitalize_first_letter__when_space_between(text, expected):
     actual = to_hashtag(text)
     assert actual == expected
