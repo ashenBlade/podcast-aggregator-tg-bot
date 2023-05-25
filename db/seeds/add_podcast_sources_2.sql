5fc2f3db92dc9c129f98d5031cd4553c238de4ca
ash.blade <ashen.blade.dev@gmail.com>
2023-05-25 18:52:24 +0300

    Исправил id гугл подкаста в для The DevOps Kitchen Talks

--- db/seeds/add_podcast_sources_2.sql
+++ db/seeds/add_podcast_sources_2.sql
@@ -7,7 +7,7 @@ values
 ('Цинковый прод', '9649473', 'aHR0cHM6Ly9mZWVkcy5zb3VuZGNsb3VkLmNvbS91c2Vycy9zb3VuZGNsb3VkOnVzZXJzOjYxMDU4MjU1Ny9zb3VuZHMucnNz', 'id1458311254'),
 ('Сделайте мне красиво', '6880172', 'aHR0cDovL2ZlZWRzLmZlZWRidXJuZXIuY29tL2JlZWdlYm90L3Ntaw', 'id1447645563'),
 ('ПроUI 5', '12115451', 'aHR0cDovL3Jzcy5jYXN0Ym94LmZtL2V2ZXJlc3QvMWJjYzI5ZjdjYTkwNDI4Yzg3NmUzZDc1NTAwMTg0YzEueG1s', 'id1531586151'),
-('The DevOps Kitchen Talks''s Podcast', '10151746', '/aHR0cHM6Ly9mZWVkLnBvZGJlYW4uY29tL2Rldm9wc2tpdGNoZW50YWxrcy9mZWVkLnhtbA', 'id1502639464'),
+('The DevOps Kitchen Talks''s Podcast', '10151746', 'aHR0cHM6Ly9mZWVkLnBvZGJlYW4uY29tL2Rldm9wc2tpdGNoZW50YWxrcy9mZWVkLnhtbA', 'id1502639464'),
 ('Базовый Блок: подкаст про блокчейн', '7638583', 'aHR0cHM6Ly9iYXNpY2Jsb2NrcmFkaW8ubGlic3luLmNvbS9yc3M', 'id1289965366'),
 ('Игрожоры', '6965497', 'aHR0cHM6Ly9wb2RzdGVyLmZtL3Jzcy54bWw_cGlkPTYxNjA3', 'id1438708331'),
 ('Scalalaz Podcast', '9416326', 'aHR0cHM6Ly9zY2FsYWxhei5ydS9yc3MvZmVlZC54bWw', 'id1156356598'),
@@ -18,5 +18,5 @@ values
 ('Mobile People Talks', '9647763', 'aHR0cHM6Ly9mZWVkcy5zb3VuZGNsb3VkLmNvbS91c2Vycy9zb3VuZGNsb3VkOnVzZXJzOjYwNzU5Mzg4Mi9zb3VuZHMucnNz', 'id1457942497'),
 ('Appbooster', null, null, 'id1481440666'),
 ('#DroiderCast', '9048349', 'aHR0cHM6Ly9wb2RzdGVyLmZtL3Jzcy54bWw_cGlkPTQxOTcx', 'id1057019371');
-
+ 
 commit;
